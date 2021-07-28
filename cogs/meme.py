f45aab53c86ce1303dd3bcfb250d9023e934e7d3
Anthony Co <anthonyashco@gmail.com>
2021-07-28 09:06:49 -0700

    Attempt iseven getter.

--- cogs/meme.py
+++ cogs/meme.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import Bot, Cog, Context, command, guild_only
 from helpers.basics import say
 from is_even import is_even
+import requests
 
 
 class Meme(Cog):
@@ -18,6 +19,20 @@ class Meme(Cog):
                 message = f"{query} is even."
             else:
                 message = f"{query} is not even."
+        except Exception as e:
+            message = str(e)
+        await say(ctx.channel, message)
+
+    @command()
+    @guild_only()
+    async def isss_even(self, ctx: Context, query: int):
+        try:
+            r = requests.get(url=f"https://api.isevenapi.xyz/api/{query}/")
+            ie = r.json()
+            if ie["iseven"]:
+                message = f"{query} is even.\n\n{ie['ad']}"
+            else:
+                message = f"{query} is not even.\n\n{ie['ad']}"
         except Exception as e:
             message = str(e)
         await say(ctx.channel, message)
