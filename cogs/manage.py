af1a32ffbf9a2f1b51dcc8feec6ff1e79df39d3f
Anthony Co <anthonyashco@gmail.com>
2021-08-05 15:38:53 -0700

    Convert to slash; add permissions checks.

--- cogs/manage.py
+++ cogs/manage.py
@@ -1,9 +1,25 @@
 """Bot management functions for the bot owner."""
 
-from discord.ext.commands import Bot, Cog, Context, command, group, guild_only, is_owner
+from __main__ import settings
+from discord.ext.commands import Bot, Cog
+from discord_slash import SlashContext as ctx
+from discord_slash.cog_ext import cog_subcommand
+from discord_slash.model import SlashCommandPermissionType as perm
+from discord_slash.model import SlashCommandOptionType as opt
+from discord_slash.utils.manage_commands import create_option, create_permission
 from helpers.basics import say
 import os
 import subprocess
+
+guild_ids = settings["slash"]["guilds"]
+owners = settings["owners"]
+base_permissions = {}
+
+for guild in guild_ids:
+    base_permissions[guild] = []
+    for owner in owners:
+        base_permissions[guild].append(create_permission(
+            owner, perm.USER, True))
 
 
 class Manage(Cog):
@@ -11,17 +27,14 @@ class Manage(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @group(aliases=["git"])
-    @guild_only()
-    @is_owner()
-    async def manage(self, ctx: Context):
-        """The main manage group. git is an alias purely for typing git pull."""
-        if ctx.invoked_subcommand is None:
-            await say(ctx.channel, "Forgot how to use it?")
-
-    @manage.command()
-    async def pull(self, ctx: Context):
+    @cog_subcommand(base="manage",
+                    base_desc="Bot management group.",
+                    base_default_permission=False,
+                    base_permissions=base_permissions,
+                    guild_ids=guild_ids)
+    async def pull(self, ctx: ctx):
         """Performs git pull in the terminal."""
+        await ctx.defer()
         try:
             if ".git" in os.listdir():
                 p = subprocess.run("git pull",
@@ -29,12 +42,16 @@ class Manage(Cog):
                                    check=True,
                                    shell=True,
                                    text=True)
-                await say(ctx.channel, p.stdout)
+                await say(ctx, p.stdout)
         except subprocess.CalledProcessError:
-            await say(ctx.channel, "This ain't it, chief.")
+            await say(ctx, "This ain't it, chief.")
 
-    @manage.command()
-    async def reload(self, ctx: Context, cog: str = None):
+    @cog_subcommand(
+        base="manage",
+        description="Reloads a cog after pulling updates.",
+        guild_ids=guild_ids,
+        options=[create_option("cog", "The cog to update", opt.STRING, True)])
+    async def reload(self, ctx: ctx, cog: str):
         """Reloads a cog after pulling updates.
 
         Args:
@@ -43,13 +60,17 @@ class Manage(Cog):
         if cog is not None:
             try:
                 self.bot.reload_extension("cogs." + cog)
-                await say(ctx.channel, f"The {cog} cog was reloaded.")
+                await say(ctx, f"The {cog} cog was reloaded.")
             except Exception as e:
-                print(e)
-                await say(ctx.channel, "Something went wrong...")
+                print(f"{type(e).__name__}: {e}")
+                await say(ctx, "Something went wrong...")
 
-    @manage.command()
-    async def load(self, ctx: Context, cog: str = None):
+    @cog_subcommand(
+        base="manage",
+        description="Load a new cog.",
+        guild_ids=guild_ids,
+        options=[create_option("cog", "The cog to load.", opt.STRING, True)])
+    async def load(self, ctx: ctx, cog: str = None):
         """Load a new cog.
 
         Args:
@@ -58,13 +79,17 @@ class Manage(Cog):
         if cog is not None:
             try:
                 self.bot.load_extension("cogs." + cog)
-                await say(ctx.channel, f"The {cog} cog was loaded.")
+                await say(ctx, f"The {cog} cog was loaded.")
             except Exception as e:
-                print(e)
-                await say(ctx.channel, "Something went wrong...")
+                print(f"{type(e).__name__}: {e}")
+                await say(ctx, "Something went wrong...")
 
-    @manage.command()
-    async def unload(self, ctx: Context, cog: str = None):
+    @cog_subcommand(
+        base="manage",
+        description="Unload an existing cog.",
+        guild_ids=guild_ids,
+        options=[create_option("cog", "The cog to unload.", opt.STRING, True)])
+    async def unload(self, ctx: ctx, cog: str = None):
         """Unload an existing cog.
 
         Args:
@@ -73,17 +98,15 @@ class Manage(Cog):
         if cog is not None:
             try:
                 self.bot.unload_extension("cogs." + cog)
-                await say(ctx.channel, f"Alright, the {cog} cog was unloaded.")
+                await say(ctx, f"Alright, the {cog} cog was unloaded.")
             except Exception as e:
-                print(e)
-                await say(ctx.channel, "Something went wrong...")
+                print(f"{type(e).__name__}: {e}")
+                await say(ctx, "Something went wrong...")
 
-    @command()
-    @guild_only()
-    @is_owner()
-    async def exit(self, ctx: Context):
+    @cog_subcommand(base="manage", guild_ids=guild_ids)
+    async def exit(self, ctx: ctx):
         """Take a break and shut down."""
-        await say(ctx.channel, "Sure, I'll see you later.")
+        await say(ctx, "Sure, I'll see you later.")
         await self.bot.close()
         print("Client closed.")
 
