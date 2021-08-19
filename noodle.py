551eee774f3136a3842b593c27b9aaafe1815f43
Anthony Co <anthonyashco@gmail.com>
2021-08-18 17:54:25 -0700

    Minor adjustments. KeyboardInterrupt added to running loop. Token moved to before extensions for dependency.

--- noodle.py
+++ noodle.py
@@ -6,15 +6,16 @@ if __name__ == "__main__":
     connect = startup.get_settings("mongo_connect")
     bot = startup.instantiate_bot(settings)
     slash = SlashCommand(bot, sync_commands=True)
+    token = startup.get_token()
 
     extensions = startup.get_extensions(settings["optional_cogs"],
                                         settings["excluded_cogs"],
                                         settings["external_cogs"])
     [bot.load_extension(extension) for extension in extensions]
 
-    token = startup.get_token()
-
     try:
         bot.loop.run_until_complete(bot.start(token, bot=True, reconnect=True))
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received. Goodbye!")
     finally:
         bot.loop.close()
